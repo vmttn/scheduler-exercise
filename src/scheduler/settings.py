a573a518b7bf1e2b50622d0d69b4dc30fb4f7969
Valentin Matton <taq@protonmail.ch>
2021-08-13 09:44:09 +0200

    chore(test): use tmp repo folder

--- src/scheduler/settings.py
+++ src/scheduler/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+
 _CSV_REPOSITORY_FOLDER = os.environ.get("CSV_REPOSITORY_FOLDER", None)
-CSV_REPOSITORY_FOLDER = (
-    Path(_CSV_REPOSITORY_FOLDER) if _CSV_REPOSITORY_FOLDER else Path(__file__).resolve().parent.parent.parent / "data"
-)
+CSV_REPOSITORY_FOLDER = Path(_CSV_REPOSITORY_FOLDER) if _CSV_REPOSITORY_FOLDER else ROOT_DIR / "data"
